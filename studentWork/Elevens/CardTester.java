0f9df05b92b6100e79072f4659893cea42b9230e
LordOCode <hessenic000@ephrataschools.org>
2022-03-02 16:57:33 +0000

    j

---
+++ studentWork/Elevens/CardTester.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// package sampleCode.ElevensLab.Activity1;
+
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	Card Jack = new Card("Jack", "Clubs", 11);
+	}
+}
