b4cd4656af461164d84d40bbec2908e2fbd9d7c2
LordOCode <90467708+LordOCode@users.noreply.github.com>
2022-04-27 15:24:34 +0000

    j

---
+++ studentWork/fam.java
@@ -0,0 +1,16 @@
+public class fam {
+
+    public static void main(String[] args) {
+
+       
+        String[] famArray;
+
+        famArray = new String[6];
+        
+        famArray[0] = "Ben";
+
+
+
+    }
+
+}
